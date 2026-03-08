637160e812847d87cc1ad74abc2405fbcaf5da60
xjustadri <adrianamjames99@gmail.com>
2026-03-07 22:46:54 -0500

    Add files via upload

--- PartySecret/Party.js
+++ PartySecret/Party.js
@@ -5,3 +5,18 @@ let partLocation="Garden";
 let isPartySafe="false"
 
 Number.isInteger(shiftValue);
+
+
+
+const friend= "BRUTUS";
+const alphabet="abcdefghijklmnopqrstuvwxyz";
+const firstLetter= friend[0];
+const index= alphabet.indexOf(firstLetter.toLowerCase());
+const newIndex= index + shiftValue;
+const encryptedFirstLetter = alphabet[newIndex];
+const alphabetLength = alphabet.length;
+const newIndex = (index + shiftValue) % alphabetLength;
+const encryptedFirstLetter = alphabet[newIndex];
+
+const caesarCipher = "EUXWXV";
+const teaserCipher = caesarCipher.slice(0,3);
