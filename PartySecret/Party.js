89f52af25714ab56d0cdf67c2cd0736d15e153b9
xjustadri <adrianamjames99@gmail.com>
2026-03-29 11:33:42 -0400

    Add files via upload

--- PartySecret/Party.js
+++ PartySecret/Party.js
@@ -27,3 +27,54 @@ const range = 33 - 3 + 1; //31//
 const randomInRange = randomDecimal * range; //step 3//
 const randomInt = Math.floor(randomInRange);
 const shiftValue = randomInt + 3;
+
+// Exercise 4//
+const embleClue1 ="Eagle";
+const embleClue2 ="Laurel";
+const embleClue3 = 7;
+
+// step1//
+let locationStart ="";
+
+if(embleClue1 ==="Eagle")
+{
+    locationStart = "Forum";
+}
+else if (embleClue1 === "Lion")
+{
+    locationStart = "colosseum";
+}
+else
+{
+    locationStart = "Villa";
+}
+
+// step2//
+if(embleClue2 === "Laurel" && locationStart === "Forum")
+{
+    locationStart += " of Augusus";
+}
+else if (emblemClue2 === "Grapes" || locationStart === "Villa")
+{
+    locationStart += " of Pompey";
+}    
+
+// step3//
+
+switch (embleClue3)
+{
+    case 7:
+        locationStart += "North";
+        break;
+    case 3:
+        locationStart += "South";
+        break;
+    case 9:
+        locationStart += "East";
+        break;
+    case 4:
+        locationStart += "West";
+        break;
+}
+
+// Answer: === checks both value and type unlike == //
